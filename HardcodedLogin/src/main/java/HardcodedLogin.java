d052ff94c9cee8bfa8e99dcf2beb4acadf6cf7e8
Shihyun Lee <seanlee896@gmail.com>
2022-12-21 01:28:43 +0000

    Saved progress on HardcodedLogin 2022-12-21 01:28:43 +0000

--- HardcodedLogin/src/main/java/HardcodedLogin.java
+++ HardcodedLogin/src/main/java/HardcodedLogin.java
@@ -15,6 +15,28 @@ public class HardcodedLogin {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
-        return false;
+        if (username == "admin")
+        {
+            if (password == "qwerty"){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        if (username == "user")
+        {
+            if (password == "password"){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+
     }
 }
