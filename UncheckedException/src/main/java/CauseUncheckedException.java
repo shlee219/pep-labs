925de66c856892a0c850da2329c214961bca1090
Shihyun Lee <seanlee896@gmail.com>
2022-12-21 01:51:53 +0000

    Saved progress on UncheckedException 2022-12-21 01:51:53 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,7 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int[] arr = {1,2};
+        System.out.println(arr[5]);
     }
 }
