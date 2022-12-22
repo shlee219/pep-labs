6061ba40a685f2bef5c15354d7cc8803dd0f97c7
Shihyun Lee <seanlee896@gmail.com>
2022-12-22 17:33:18 +0000

    Saved progress on BankAccount 2022-12-22 17:33:18 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance = balance + amount;
     }
 
     /**
@@ -32,7 +32,13 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        
+        if( balance - amount < 0){
+            System.out.println("You cannot withdraw that much!");
+        }
+        else{
+            balance = balance - amount;
+        }
     }
 
     /**
