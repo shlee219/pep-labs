b7f03560627504a2d7fa9ba0c836fdb7dbddecb0
Shihyun Lee <seanlee896@gmail.com>
2022-12-22 05:05:00 +0000

    Saved progress on Queue 2022-12-22 05:05:00 +0000

--- Queue/src/main/java/QueueExercise.java
+++ Queue/src/main/java/QueueExercise.java
@@ -2,6 +2,7 @@
  * TODO: switch to deque
  */
 
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -28,8 +29,10 @@ public class QueueExercise {
      * implements the Queue interface, such as ArrayDeque. Deques contain all the methods required for both Queue and
      * Stack behavior.
      */
+    
     public Queue<String> createQueue(){
-        return null;
+        Queue<String> q = new PriorityQueue<String>();
+        return q;
     }
 
     /**
@@ -38,7 +41,8 @@ public class QueueExercise {
      * @return the length of queue.
      */
     public int getSize(Queue<String> queue){
-        return 0;
+       
+        return queue.size();
     }
 
     /**
@@ -48,7 +52,8 @@ public class QueueExercise {
      * @param item a String that should be added to Queue.
      */
     public void addToEndOfQueue(Queue<String> queue, String item){
-
+       
+        queue.add(item);
     }
 
     /**
@@ -58,7 +63,8 @@ public class QueueExercise {
      * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
-        return "";
+        
+        return queue.remove();
     }
 
     /**
@@ -67,6 +73,6 @@ public class QueueExercise {
      * @return the next String due to be removed (peeked) from the Queue<String> (the oldest item in the queue.)
      */
     public String getStartOfQueueWithoutRemoving(Queue<String> queue){
-        return "";
+        return queue.peek();
     }
 }
