ba8e28f1bf1f4c1c97da40c383c89bfc67334514
Shihyun Lee <seanlee896@gmail.com>
2022-12-29 04:23:21 +0000

    Saved progress on GroceryList 2022-12-29 04:23:21 +0000

--- GroceryList/src/main/java/GroceryDAO.java
+++ GroceryList/src/main/java/GroceryDAO.java
@@ -48,7 +48,7 @@ public class GroceryDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "INSERT INTO Grocery VALUES (@groceryName)";
+            String sql = "INSERT INTO Grocery VALUES ('apples')";
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.executeUpdate();
         }catch(SQLException e){
