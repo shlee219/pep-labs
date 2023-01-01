b1212e6ed9b53fc72f5a539223c96bcacaf00568
Shihyun Lee <seanlee896@gmail.com>
2023-01-01 04:28:47 +0000

    Saved progress on IsSorted 2023-01-01 04:28:47 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -1,3 +1,5 @@
+
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +9,26 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        int[] arrOld = arr;
+        for (int i = 1; i < arr.length; i++) {
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] >= arr[i]) {
+                    int plchldr = arr[j];
+                    int plchldr2 = arr[i];
+                    arr[i] = plchldr;
+                    arr[j] = plchldr2;
+                }
+            }
+        }
+            if( arrOld == arr){
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
+       
     }
-}
+
