4dd7a2fe09bc6479a9581882b3732f62bddc760c
Shihyun Lee <seanlee896@gmail.com>
2023-01-03 23:32:44 +0000

    Saved progress on WordCount 2023-01-03 23:32:44 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,15 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        int wordCount = 1;
+        for (int i = 0; i<in.length(); i++)
+        {
+           
+            if (in.charAt(i) == ' ')
+            {
+                wordCount++;
+            }
+        }
+        return wordCount;
     }
 }
