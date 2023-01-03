89d8bcd67efe117507011ebee70b7c31a817708d
Shihyun Lee <seanlee896@gmail.com>
2023-01-03 02:50:19 +0000

    Saved progress on CombineArrays 2023-01-03 02:50:19 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,4 +1,5 @@
 
+
 public class CombineArrays {
     /**
      * Combine two arrays into a single array and return it.
@@ -8,7 +9,33 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int arraySizeCount = 0;
+        for (int i = 0; i < arr1.length; i++) 
+        {
+            arraySizeCount++;
+        }
+        for (int i = 0; i < arr2.length; i++) 
+        {
+            arraySizeCount++;
+        }
 
-        return null;
+        int array[] = new int[arraySizeCount];
+        int arrayEndCount = 0;
+        
+        for (int i = 0; i < arr1.length; i++) 
+        {
+            array[i] = arr1[i];
+            arrayEndCount++;
+        }
+
+    
+        for (int j = 0; j <arr2.length; j++)
+        {
+          
+          array[arrayEndCount] = arr2[j];
+          arrayEndCount++;
+        }
+        
+        return array;
     }
 }
