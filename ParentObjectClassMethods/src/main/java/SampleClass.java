07c5c192137a25998a9ccc5856d99fbf2e4d5ed4
Shihyun Lee <seanlee896@gmail.com>
2023-01-04 00:28:47 +0000

    Saved progress on ParentObjectClassMethods 2023-01-04 00:28:47 +0000

--- ParentObjectClassMethods/src/main/java/SampleClass.java
+++ ParentObjectClassMethods/src/main/java/SampleClass.java
@@ -56,9 +56,18 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(int a, boolean b)
+    {
+        
+           return true;
+    }
+
 
 
     //    implement a custom .toString(){} method here.
-
+    public String toString()
+    {
+        
+    }
     
 }
