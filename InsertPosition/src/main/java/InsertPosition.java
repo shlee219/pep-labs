1382c31657db0b1604cacea8a5831692266adbfa
Shihyun Lee <seanlee896@gmail.com>
2023-01-05 03:35:45 +0000

    Saved progress on InsertPosition 2023-01-05 03:35:45 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -15,6 +15,14 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (target < nums[i])
+            {
+                return i;
+            }
+         
+        }
+        return nums.length;
     }
 }
