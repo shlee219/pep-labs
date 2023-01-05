6ccd7c58d622ac692e68af21242dcc50ab6f4f0b
Shihyun Lee <seanlee896@gmail.com>
2023-01-05 22:02:12 +0000

    Saved progress on ReverseAString 2023-01-05 22:02:12 +0000

--- ReverseAString/src/main/java/StringReverse.java
+++ ReverseAString/src/main/java/StringReverse.java
@@ -13,6 +13,11 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        String reversed = "";
+        for (int i = str.length()-1; i >=0; i-- )
+        {
+            reversed = reversed + str.charAt(i);
+        }
+        return reversed;
     }
 }
