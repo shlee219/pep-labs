bc4812fb708cc6a6879f4af1d9cbea8ccf6b7ee6
Shihyun Lee <seanlee896@gmail.com>
2023-01-05 01:16:58 +0000

    Saved progress on ParentObjectClassMethods 2023-01-05 01:16:58 +0000

--- ParentObjectClassMethods/src/main/java/SampleClass.java
+++ ParentObjectClassMethods/src/main/java/SampleClass.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * The test cases for this class use methods called .equals which aren't written anywhere in this class. How?
  * All objects have a method called .equals and a method called toString - they are inherited from the Object class,
@@ -58,16 +60,31 @@ public class SampleClass {
     //    implement a custom .equals(SampleClass other){} method here.
     public boolean equals(int a, boolean b)
     {
-        
-           return true;
+         this.a = a;
+         this.b = b;
+        // if (a == a && b == b){
+        //     return true;
+
+        // }
+        // else{
+        //     return false;
+        // }
+
+        if((Objects.deepEquals(a, a) == true) && (Objects.deepEquals(b, b) == true))
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 
 
-    //    implement a custom .toString(){} method here.
-    public String toString()
-    {
+    // //    implement a custom .toString(){} method here.
+    // public String toString()
+    // {
         
-    }
+    // }
     
 }
