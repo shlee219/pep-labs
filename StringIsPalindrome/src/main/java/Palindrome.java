6668cd39c30edccaa70b6e2252e8e463d3cfeadd
Shihyun Lee <seanlee896@gmail.com>
2023-01-06 02:06:46 +0000

    Saved progress on StringIsPalindrome 2023-01-06 02:06:46 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,17 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        String reversed = "";
+        for (int i = str.length()-1; i >=0; i-- )
+        {
+            reversed = reversed + str.charAt(i);
+        }
+
+        // if(reversed == str)
+        // {
+        //     return true;
+        // }
+        
+        return reversed.equals(str);
     }
 }
