84f5d0a9ab1b99260bb5ec630e823a306104a15f
Shihyun Lee <seanlee896@gmail.com>
2023-01-06 00:20:15 +0000

    Saved progress on MostCommonCharacter 2023-01-06 00:20:15 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -13,22 +13,37 @@ public class MostCommonCharacter {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
         Map.Entry<Character, Integer> maxValue = null;
         
-        for (int i = 0; i < str.length(); i++) {
+        //Loop through given string "str"
+        for (int i = 0; i < str.length(); i++) 
+        {
+            // char "c" holds current character at index(i) of "str"
             char c = str.charAt(i);
+            //int "val" will hold the current Key-Value pair associated with "c"
             Integer val = map.get(c);
-            if (val != null) {
+
+            //If value of current character "c" is null, give it value of 1. 
+                //Or easier said, if current character(key) in the string has not been counted before set the value of it to 1
+                    //If it isn't null, increment the value by 1 (If it has been counted before, up the counter by 1)
+            if (val != null) 
+            {
                 map.put(c, val + 1);
-            } else {
+            } else 
+            {
                 map.put(c, 1);
             }
         }
-        System.out.println(map);
-        for (Map.Entry<Character, Integer> value : map.entrySet()) {
-            if (maxValue == null || value.getValue().compareTo(maxValue.getValue()) > 0) {
+        
+        //Loop through the hashmap "map"
+        for (Map.Entry<Character, Integer> value : map.entrySet()) 
+        {
+            //If maxValue is null (probably because the loop has just begun), set the value associated with current loop's key as maxValue
+            //Or if current value of this iteration is bigger than what maxValue is, set that as maxValue.
+            if (maxValue == null || value.getValue().compareTo(maxValue.getValue()) > 0) 
+            {
                 maxValue = value;
             }
         }
-        
+        //return only the key from the key-value pair that has the highest value.
         return maxValue.getKey();
     }
 }
