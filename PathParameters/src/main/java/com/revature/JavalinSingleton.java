f1e7c2706cce8fe56719917ede860709f875219f
Shihyun Lee <seanlee896@gmail.com>
2023-02-08 19:13:44 +0000

    Saved progress on PathParameters 2023-02-08 19:13:44 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -16,8 +16,8 @@ public class JavalinSingleton {
          */
         app.get("/firstname/{first}", ctx -> {
             
-            //write code here
-
+            String firstName = ctx.pathParam("first");
+            ctx.result(firstName);
         });
 
 
