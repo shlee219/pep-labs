59aec54930ae5769a0a20a00b210c0c6efc1fb69
Shihyun Lee <seanlee896@gmail.com>
2023-02-09 00:06:52 +0000

    Saved progress on ForeignKey 2023-02-09 00:06:52 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,7 +1,6 @@
-CREATE TABLE post ( 
-id SERIAL PRIMARY KEY, 
-post varchar(255), 
-user_fk int REFERENCES site_user(id) 
+CREATE TABLE post (
+  id SERIAL PRIMARY KEY,
+  post VARCHAR(255),
+  user_fk INT,
+  FOREIGN KEY (user_fk) REFERENCES site_user(id)
 );
-
-
